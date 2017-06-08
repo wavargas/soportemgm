de99b4bbc2d4dbc08fe701f7ad876b5e498e4da8
William Vargas <William Vargas@wvargas>
2017-06-08 17:21:50 -0500

    Inicio Proyecto

---
+++ config/config_inc.php
@@ -0,0 +1,25 @@
+<?php
+
+define ('HOST', "localhost");
+define ('DATABASE_NAME', "mantis_mgms");
+define ('USERNAME', "root");
+define ('PASSWORD', "");
+
+$g_hostname               = HOST;
+$g_db_type                = 'mysqli';
+$g_database_name          = DATABASE_NAME;
+$g_db_username            = USERNAME;
+$g_db_password            = PASSWORD;
+/*
+$g_hostname               = 'localhost';
+$g_db_type                = 'mysqli';
+$g_database_name          = 'mantis_mgms';
+$g_db_username            = 'root';
+$g_db_password            = '';
+*/
+$g_db_table_prefix          = 'mgms';
+
+$g_default_timezone       = 'America/Bogota';
+
+$g_crypto_master_salt     = 'lv+5Nnh48bwXz2N3wcdNHrj2UnnDgSr2cXSYrMwsMTM=';
+
